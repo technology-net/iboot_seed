be7da262e971c402008e742f00e076b6e8c09d8a
toi.xtran <toi.xtran@gmail.com>
2023-11-27 21:55:17 +0700

    add core package

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        IBoot\Core\App\Providers\CoreServiceProvider::class,
     ])->toArray(),
 
     /*
