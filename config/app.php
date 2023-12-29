bc53336a4a7c17a5a78b43980020e92a2545e724
ninhnk <ninhnk@icitech.net>
2023-12-29 09:38:42 +0700

    remove packages iboot/core to composer json

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        IBoot\Core\App\Providers\CoreServiceProvider::class,
     ])->toArray(),
 
     /*
